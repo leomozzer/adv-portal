cb4bd9ab50eaeb2aee115e894236e58c548a2ee1
leomozzer <mozzer.ferraz@gmail.com>
2023-09-12 13:54:39 -0300

    Changed sto declaration

--- terraform-live/storage-account.tf
+++ terraform-live/storage-account.tf
@@ -1,13 +1,22 @@
-# resource "azurerm_storage_account" "avd-storage-account" {
-#   name                     = local.avd_storage_account_name
-#   location                 = azurerm_resource_group.resource_group.location
-#   resource_group_name      = azurerm_resource_group.resource_group.name
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
-# }
+resource "azurerm_storage_account" "avd_storage_account" {
+  name                     = local.avd_storage_account
+  location                 = azurerm_resource_group.resource_group.location
+  resource_group_name      = azurerm_resource_group.resource_group.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  access_tier              = "Cool"
+}
 
-# resource "azurerm_storage_queue" "queue" {
-#   for_each             = toset(var.queue_list)
-#   name                 = each.value
-#   storage_account_name = azurerm_storage_account.avd-storage-account.name
-# }
+resource "azurerm_storage_container" "scripts" {
+  name                  = "scripts"
+  storage_account_name  = azurerm_storage_account.avd_storage_account.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "domainjoin" {
+  name                   = "domainjoin.ps1"
+  storage_account_name   = azurerm_storage_account.avd_storage_account.name
+  storage_container_name = azurerm_storage_container.scripts.name
+  type                   = "Block"
+  source                 = "../scripts/domainjoin.ps1"
+}
