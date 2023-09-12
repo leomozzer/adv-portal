07e3540e9f89e72a900826dc26ce1cdfb349ddfb
leomozzer <mozzer.ferraz@gmail.com>
2023-09-12 13:53:45 -0300

    Added app insights

--- terraform-live/avd-portal.tf
+++ terraform-live/avd-portal.tf
@@ -6,19 +6,28 @@ resource "azurerm_resource_group" "resource_group_portal" {
   }
 }
 
-resource "azurerm_storage_account" "avd-storage-account-portal" {
-  name                     = local.avd_storage_account_name
+resource "azurerm_storage_account" "storage_account_portal" {
+  name                     = local.avd_storage_account_portal
   location                 = azurerm_resource_group.resource_group_portal.location
   resource_group_name      = azurerm_resource_group.resource_group_portal.name
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  access_tier              = "Cool"
 }
 
 resource "azurerm_storage_queue" "queue" {
   for_each             = toset(var.queue_list)
   name                 = each.value
-  storage_account_name = azurerm_storage_account.avd-storage-account-portal.name
+  storage_account_name = azurerm_storage_account.storage_account_portal.name
 }
+
+resource "azurerm_application_insights" "application_insights" {
+  name                = local.application_insights_portal
+  resource_group_name = azurerm_resource_group.resource_group_portal.name
+  location            = azurerm_resource_group.resource_group_portal.location
+  application_type    = "web"
+}
+
 
 resource "azurerm_service_plan" "func_app" {
   name                = local.service_plan_portal
@@ -29,18 +38,24 @@ resource "azurerm_service_plan" "func_app" {
 }
 
 resource "azurerm_windows_function_app" "func_app_portal" {
-  name                = local.function_app
+  name                = local.function_app_portal
   resource_group_name = azurerm_resource_group.resource_group_portal.name
   location            = azurerm_resource_group.resource_group_portal.location
 
-  storage_account_name       = azurerm_storage_account.avd-storage-account-portal.name
-  storage_account_access_key = azurerm_storage_account.avd-storage-account-portal.primary_access_key
+  storage_account_name       = azurerm_storage_account.storage_account_portal.name
+  storage_account_access_key = azurerm_storage_account.storage_account_portal.primary_access_key
   service_plan_id            = azurerm_service_plan.func_app.id
-
   site_config {
     application_stack {
-      powershell_core_version = 7
+      powershell_core_version = "7"
     }
+  }
+
+  app_settings = {
+    "FUNCTIONS_WORKER_RUNTIME"       = "powershell"
+    "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.application_insights.instrumentation_key
+    "STORAGE_ACCOUNT_NAME"           = azurerm_storage_account.storage_account_portal.name
+    "STORAGE_ACCOUNT_KEY"            = azurerm_storage_account.storage_account_portal.primary_access_key
   }
 
   identity {
@@ -48,8 +63,23 @@ resource "azurerm_windows_function_app" "func_app_portal" {
   }
 }
 
-resource "azurerm_role_assignment" "mi_func_app" {
-  scope                = azurerm_resource_group.resource_group_portal.id
+#Fix PowerShel version issue
+#https://github.com/hashicorp/terraform-provider-azurerm/issues/8867
+resource "null_resource" "fix_powershell_version" {
+  provisioner "local-exec" {
+    command = <<-EOT
+      
+ az functionapp update --name ${azurerm_windows_function_app.func_app_portal.name} --resource-group ${azurerm_resource_group.resource_group_portal.name} --set siteConfig.powerShellVersion=7.2
+    EOT
+  }
+  depends_on = [azurerm_windows_function_app.func_app_portal]
+  triggers = {
+    build_number = "1"
+  }
+}
+
+resource "azurerm_role_assignment" "mi_resource_group_portal" {
+  scope                = data.azurerm_subscription.current.id
   role_definition_name = "Contributor"
   principal_id         = azurerm_windows_function_app.func_app_portal.identity[0].principal_id
 }
